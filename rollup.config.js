6dfa3bf410af23bdc0c4789a6014c59e53abed77
qhbin <925078922@qq.com>
2025-09-24 12:25:39 +0800

    fix: 修复旧版本浏览器不兼容问题

--- rollup.config.js
+++ rollup.config.js
@@ -9,23 +9,26 @@ import livereload from 'rollup-plugin-livereload';
 const isProduction = process.env.NODE_ENV === 'production';
 
 export default {
-  input: './node_modules/react-router-dom/dist/index.js',
+  input: './node_modules/@sentry/react/build/esm/index.js',
   output: {
-    file: 'dist/react-router-dom.umd.js',
+    file: 'dist/sentry-react.umd.js',
     format: 'umd',
-    name: 'ReactRouterDOM',
+    name: 'SentryReact',
     globals: {
       react: 'React',
       'react-dom': 'ReactDOM',
     },
     sourcemap: true,
-    exports: 'auto'
+    exports: 'auto',
+    // 在产物首行添加globalThis polyfill
+    banner: '// globalThis polyfill for older browsers\nif (typeof globalThis === "undefined") { var globalThis = self || window || global; }\n'
   },
   external: [
     'react',
     'react-dom',
   ],
   plugins: [
+    // 在plugins数组中的replace配置修改为：
     replace({
       'process.env.NODE_ENV': JSON.stringify('production'),
       'process.env': '{}',
@@ -35,12 +38,17 @@ export default {
       browser: true
     }),
     commonjs(),
+    // 替换当前的 babel 配置
     babel({
       babelHelpers: 'runtime',
       presets: [
-        ['react-app', { targets: { browsers: ['> 0.5% in CN', 'last 2 versions'] } }]
+        ['@babel/preset-env', { targets: { browsers: ['> 0.5% in CN', 'last 2 versions'] } }]
+      ],
+      plugins: [
+        '@babel/plugin-transform-runtime'
       ]
     }),
+    
     // 只在开发模式下启用 serve 和 livereload
     !isProduction && serve({
       port: 3000,
